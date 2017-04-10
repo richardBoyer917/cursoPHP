483dc26932b2c70d1cbb7cf2c88bef333aaed069
richardBoyer917 <jameshenson005@gmail.com>
2017-04-10 14:14:24 -0500

    Added operadores_aritmeticos.php

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,27 @@
+<?php
+  //suma
+  $a = 5; $b = 2;
+  echo $a + $b;
+  echo "<p></p>";
+  //resta
+  echo $a - $b;
+  echo "<p></p>";
+  //multiplicación
+  echo $a * $b;
+  echo "<p></p>";
+  //división
+  echo $a / $b;
+  echo "<p></p>";
+  //exponenciación
+  $res = pow($b, $a);
+  echo $res;
+  echo "<p> </p>";
+  //módulo
+  echo $a % $b;
+  echo "<p></p>";
+  //Identidad y negación
+  $prueba = 2;
+  $identidad = +$prueba;
+  echo "<p></p>";
+  $negacion = -$prueba;
+ ?>
