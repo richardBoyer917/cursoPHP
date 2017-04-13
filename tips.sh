387050d9a1a94abdd6ca36705a13fd60fd12eef2
richardBoyer917 <jameshenson005@gmail.com>
2017-04-13 17:33:48 -0500

    Added tips.sh

---
+++ tips.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Para abrir un archivo con atom cuando estamos en x directorio:"
+echo "$ atom archivo.aa"
+echo "O varios"
+echo "$ atom archivo1.aa archivo2.aa ..."
+echo "Igual podemos usar el asterísco cuando son del mismo formato"
+echo "$ atom *.*"
+
